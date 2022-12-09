2bc9ec63a9a3c8f80114915c8c29a4331b579edd
virus567 <stud126197@vyatsu.ru>
2022-12-09 15:39:42 +0300

    add tests

---
+++ gRPCTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace gRPCTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
